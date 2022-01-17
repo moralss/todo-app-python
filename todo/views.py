a3337907fb1bdcf731db289fef6414c8fc5e7fe9
moral-devson <moral@devson.co.za>
2022-01-17 21:33:56 +0200

    Added code to show list of todos

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+todos_array = []
+class todo():
+  def __init__(self , name):
+    self.name = name
+
+todos_array.append(todo('clean my room '))
+todos_array.append(todo('play soccer '))
+
 def say_hello (request):
-  return HttpResponse('Hello world')
+  print( todos_array[0].name)
+  return  render(request , 'todos.html' , {'todos_array' : todos_array})
+
+
+# def post_list(request, template_name='todos.html'):
+#     # posts = blog_posts.objects.all()
+#     # data = {}
+#     # data['object_list'] = posts
+#     return render(request, template_name, data)
