4fee4a4eb73cc075ea1330207543e440db2848f9
moral-devson <moral@devson.co.za>
2022-01-18 12:56:26 +0200

    Completed functionality to delete a todo

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('showTodo/', views.show_todo),
-    path('addTodo/', views.post_todo)
+    path('showTodo/', views.show_todo, name='show_todo'),
+    path('addTodo/', views.post_todo),
+    # path(r'^delete/(?P<pk>[0-9]+)/$', views.todo_delete, name='todo_delete')
+    path('todoDelete/<int:id>/', views.todo_delete,  name='todo_delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,24 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import TodoForm
 from django.shortcuts import render, redirect, get_object_or_404
-
+import random
 # Create your views here.
 
 todos_array = []
+selected_id = 0
 
 
-class todo():
-    def __init__(self, topic, description):
+class Todo():
+    def __init__(self, id,  topic, description):
+        self.id = id
         self.topic = topic
         self.description = description
 
 
-todos_array.append(todo('work',  'clean room'))
-todos_array.append(todo('play', 'play fifa'))
+todos_array.append(Todo(1, 'work',  'clean room'))
+todos_array.append(Todo(2, 'play', 'play fifa'))
 
 
 def show_todo(request):
-    print(todos_array[0].topic)
     return render(request, 'todos.html', {'todos_array': todos_array})
 
 
@@ -31,8 +32,20 @@ def post_todo(request):
 
             topic = form.cleaned_data['topic']
             descrption = form.cleaned_data['descrption']
-            todos_array.append(todo(topic, descrption))
-            return redirect('/todo/show_todo/')
+            todos_array.append(
+                Todo(random.randint(0, 100000), topic, descrption))
+            return redirect('/todo/showTodo/')
 
     form = TodoForm()
     return render(request, 'addTodo.html', {'form': form})
+
+
+def todo_delete(request, id):
+    if request.method == 'POST':
+        for item in todos_array:
+            print(request)
+            if item.id == id:
+                print("I found what I was looking for")
+                todos_array.remove(item)
+                return redirect('/todo/showTodo/')
+    return render(request, 'delete.html', {})
